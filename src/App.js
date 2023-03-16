36c31f60b8461ff482354fd734d8f63d31d168f4
SoonYu97 <yusoon.97417@gmail.com>
2023-03-16 10:12:18 +0800

    fix: add reference to input tag

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useState, useRef } from "react"
 import "./App.css"
 
 function App() {
@@ -7,7 +7,17 @@ function App() {
 	const [date, setDate] = useState("00/00")
 	const [cvc, setCvc] = useState("000")
 
+	const nameRef = useRef()
+	const numberRef = useRef()
+	const monthRef = useRef()
+	const yearRef = useRef()
+	const cvcRef = useRef()
+
 	const Confirm = (e) => {
+		setName(nameRef.current.value)
+		setNumber(numberRef.current.value)
+		setDate(`${monthRef.current.value}/${yearRef.current.value}`)
+		setCvc(cvcRef.current.value)
 		e.preventDefault()
 	}
 
@@ -26,25 +36,51 @@ function App() {
 			<section className="Form-section">
 				<form onSubmit={Confirm}>
 					<label htmlFor="name">
-						Fruit:
-						<input type="text" id="name" placeholder="e.g. Jane Appleseed" />
+						Cardholder Name
+						<input
+							required
+							type="text"
+							id="name"
+							placeholder="e.g. Jane Appleseed"
+							ref={nameRef}
+						/>
 					</label>
 					<label htmlFor="number">
-						Fruit:
+						Card Number
 						<input
+							required
 							type="number"
 							id="number"
 							placeholder="e.g. 1234 5678 9123 0000"
+							ref={numberRef}
 						/>
 					</label>
 					<label htmlFor="date">
-						Fruit:
-						<input type="number" id="date" placeholder="MM" />
-						<input type="number" id="date" placeholder="YY" />
+						Exp. Date (MM/YY)
+						<input
+							required
+							type="number"
+							id="date"
+							placeholder="MM"
+							ref={monthRef}
+						/>
+						<input
+							required
+							type="number"
+							id="date"
+							placeholder="YY"
+							ref={yearRef}
+						/>
 					</label>
 					<label htmlFor="cvc">
-						Fruit:
-						<input type="number" id="cvc" placeholder="e.g. 123" />
+						CVC
+						<input
+							required
+							type="number"
+							id="cvc"
+							placeholder="e.g. 123"
+							ref={cvcRef}
+						/>
 					</label>
 					<button className="Confirm-button" type="submit">
 						Confirm
